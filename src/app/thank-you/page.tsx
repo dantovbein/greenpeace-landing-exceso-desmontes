b0711a62aec1a2fd3e75b649746a1f95ef279147
Dan Tovbein <dtovbein@gmail.com>
2024-07-29 17:10:12 -0300

    Implement form submission

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,10 +1,21 @@
 'use client'
 
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import ThankYou from '@/app/_components/ThankYou'
 import HomeText from "../_components/HomeText";
 
+declare const window: Window & { dataLayer: Record<string, unknown>[]; };
+
 const Page:FC<{}> = () => {
+
+  useEffect(() => {
+    if(window.dataLayer) {
+      window.dataLayer.push({
+        event: "formSubmission",
+      });
+    }
+  }, [])
+
   return (
     <>
       <ThankYou />
